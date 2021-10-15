aba230db7eb84b11b8cff47b1ad9fd6281202163
WBH <1762629204@qq.com>
2021-10-15 22:27:22 +0800

    update gitignore

---
+++ hw/hw07/hw06.scm
@@ -0,0 +1,26 @@
+(define (cddr s)
+  (cdr (cdr s)))
+
+(define (cadr s)
+  'YOUR-CODE-HERE
+  (car (cdr s))
+)
+
+(define (caddr s)
+  'YOUR-CODE-HERE
+   (car (cddr s))
+)
+
+
+(define (sign num)
+  'YOUR-CODE-HERE
+  (cond ())
+)
+
+
+(define (square x) (* x x))
+
+(define (pow x y)
+  'YOUR-CODE-HERE
+)
+
